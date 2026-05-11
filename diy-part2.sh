8b68a1c571ca9f4f37d6fa55b235bbc8713b35b8
ljkk-li <51225555+ljkk-li@users.noreply.github.com>
2026-05-11 22:45:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ rm -rf feeds/luci/themes/luci-theme-argon
 
 
 #添加额外软件包
-git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-24.10 feeds/packages/lang/node 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
 git clone --depth=1 https://github.com/vernesong/OpenClash.git
