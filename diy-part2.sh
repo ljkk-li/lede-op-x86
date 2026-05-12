f5c896bb76fd93847536046c0509782192f0a119
ljkk-li <51225555+ljkk-li@users.noreply.github.com>
2026-05-13 07:57:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,7 +37,7 @@ rm -rf kenzok8-packages
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.99.2/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.99.2/g' package/base-files/luci2/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.99.2/g' package/base-files/luci2/bin/config_generate
 
 #readd cpufreq for aarch64
 #sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
